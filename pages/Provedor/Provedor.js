c0473ed3732f23c3aeaafce30625fa14c8dcc484
Orlin Perez <perezorlin104@gmail.com>
2021-01-22 10:36:40 -0400

    modal-funcionar

--- pages/Provedor/Provedor.js
+++ pages/Provedor/Provedor.js
@@ -1,10 +1,11 @@
+import {enviarDatoProveedor} from '../../firebase.BD/ProvedorDB';
 import { useState, useEffect } from "react";
 import * as React from "react";
 import {  listenData } from "../../firebase.BD/ProvedorDB";
 import Button from "@material-ui/core/Button";
 import GetAppIcon from "@material-ui/icons/GetApp";
 import Modal from "@material-ui/core/Modal";
-import {FormsProveedor} from './FormsProveedor'
+// import {FormsProveedor} from './FormsProveedor'
 import { TableContainer, TableCell ,TableHead,Table, TableRow, TableBody, ButtonGroup, Fab } from "@material-ui/core";
 import Paper from '@material-ui/core/Paper';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -12,7 +13,6 @@ import EditIcon from '@material-ui/icons/Edit';
 
 export default function Provedor() {
   const [open, setModal] = useState(false);
-  const [openDeleteModal, setOpenDeleteModal] = useState(false);
   const [ rows, setRows ] = useState([ ]);
   
    useEffect(()=>{
@@ -23,8 +23,8 @@ export default function Provedor() {
   const openModal = () => {
     setModal(true)
   };
-  const alertDelete =()=>{
-
+  const closeModal =()=>{
+    setModal(false)
   }
   
   const search = ()=>{
@@ -34,6 +34,93 @@ export default function Provedor() {
     console.log(rows)
     console.log(found)
   }
+const proveedorInputs = () => {
+  const nombreProveedor = document.getElementById("nombreProveedor").value;
+  const dirrecion = document.getElementById("dirrecion").value;
+  const cargoRepresentante = document.getElementById("cargoRepresentante").value;
+  const telefono = document.getElementById("telefono").value;
+  const emailProveedor = document.getElementById("email").value;
+  const terminosPago = document.getElementById("terminosPago").value;
+  const notaProveedor = document.getElementById("notaProveedor").value;
+  enviarDatoProveedor(
+    emailProveedor,
+    terminosPago,
+    notaProveedor,
+    telefono,
+    dirrecion,
+    cargoRepresentante,
+    nombreProveedor
+  );
+  setModal(false)
+};
+
+const FormsProveedor = (
+  <form className="form-modal">
+    <h1>Añadir proveedor</h1>
+    <div>
+      <label>
+        <input
+          type="text"
+          id="nombreProveedor"
+          placeholder="Nombre del proveedor"
+          required
+        />
+      </label>
+      <label>
+        <input
+          type="text"
+          id="terminosPago"
+          placeholder="Terminos de pago"
+          required
+        />
+      </label>
+    </div>
+    <div>
+      <label>
+        <input
+          type="text"
+          id="dirrecion"
+          placeholder="Dirrecion del proveedor"
+          required
+        />
+      </label>
+      <label>
+        <input
+          type="text"
+          id="cargoRepresentante"
+          placeholder="Cargo del Representante"
+          required
+        />
+      </label>
+    </div>
+    <div>
+      <label>
+        <input type="text" id="telefono" placeholder="Telefono" required />
+      </label>
+      <label>
+        <input type="email" id="email" placeholder="Email" required />
+      </label>
+    </div>
+
+    <div>
+      <textarea id="notaProveedor" placeholder="Añadir nota..."></textarea>
+    </div>
+    <div>
+      <Button onClick={closeModal} variant="contained" color="secondary">
+        Cancelar
+      </Button>
+      &nbsp; &nbsp;
+      <Button
+        variant="contained"
+        onClick={proveedorInputs}
+        onKeyPress={proveedorInputs}
+        color="primary"
+      >
+        Añadir
+      </Button>
+    </div>
+  </form>
+);
 
   return (
     <>
@@ -89,9 +176,10 @@ export default function Provedor() {
               <TableCell align="left">{row.notaProveedor}</TableCell>
               <TableCell>
                   <button  data-id={row.uId} >
+           
                     <EditIcon color="primary"/>
                   </button>
-                  <button data-id={row.uId} onClick={alertDelete} >
+                  <button data-id={row.uId}  >
                     <DeleteIcon color="secondary"/>
                   </button>
                   
